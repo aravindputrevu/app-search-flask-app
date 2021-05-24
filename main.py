770799cd2c61794a5a3b034dc76342d7cc97baa2
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-05-24 10:14:11 +0530

    Consuming Ent search library changes

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 from flask import Flask, render_template, request     
-from elastic_app_search import Client
+from elastic_enterprise_search import AppSearch
+from elasticapm.contrib.flask import ElasticAPM
 import json
+import logging
 
 app = Flask(__name__)
 
 with open("config.json") as json_data_file:
     config = json.load(json_data_file)
 
-client = Client(
-    base_endpoint=config['appsearch']['base_endpoint'],
-    api_key=config['appsearch']['api_key'],
-    use_https=True)
+apm = ElasticAPM(app,logging=logging.ERROR,
+server_url=config['apm']['server_url'] ,
+ service_name=config['apm']['service_name'], 
+ secret_token=config['apm']['secret_token'])
+
+client = AppSearch(
+    config['appsearch']['base_endpoint'],
+    http_auth=config['appsearch']['api_key'])
 
 engine_name = config['appsearch']['engine_name']
 
 @app.route("/")
 def home():
-    data = client.search(engine_name, "", {})
+    data = client.search(engine_name, body={
+        "query": ""
+    })
     return render_template("home.html" , data=data)
 
 @app.route("/search", methods = ['POST'])
 def search():
     if request.method == 'POST':
       query = request.form['search']
-    data = client.search(engine_name, query, {})
+    data = client.search(engine_name, body={
+        "query": query
+    })
     return render_template("results.html" , data=data)
-     
-@app.route("/index")
-def index():
-    # Opening JSON file 
-    f = open('data.json',) 
-
-    # returns JSON object as  
-    # a dictionary 
-    documents = json.load(f)
-    data = client.index_documents(engine_name, documents)
-    return render_template("about.html" , data=data)
     
 if __name__ == "__main__":
     app.run(debug=False)
