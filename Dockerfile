667227cc9aadea910cddbce910cbd148d706e736
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-08-25 11:41:28 +0530

    Code changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-# For more information, please refer to https://aka.ms/vscode-docker-python
 FROM python:3.8-slim-buster
 
 EXPOSE 5000
@@ -16,9 +15,7 @@ RUN python -m pip install -r requirements.txt
 WORKDIR /app
 ADD . /app
 
-# Switching to a non-root user, please refer to https://aka.ms/vscode-docker-python-user-rights
 RUN useradd appuser && chown -R appuser /app
 USER appuser
 
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
 CMD ["gunicorn", "--bind", "0.0.0.0:5000", "main:app"]
--- main.py
+++ main.py
@@ -1,41 +1,49 @@
-from flask import Flask, render_template, request     
-from elastic_app_search import Client
 import json
+
+from elastic_enterprise_search import AppSearch
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 with open("config.json") as json_data_file:
     config = json.load(json_data_file)
 
-client = Client(
-    base_endpoint=config['appsearch']['base_endpoint'],
-    api_key=config['appsearch']['api_key'],
-    use_https=True)
+client = AppSearch(
+    config['appsearch']['base_endpoint'],
+    http_auth=config['appsearch']['api_key'])
 
 engine_name = config['appsearch']['engine_name']
 
+
 @app.route("/")
 def home():
-    data = client.search(engine_name, "", {})
-    return render_template("home.html" , data=data)
+    data = client.search(engine_name, body={
+        "query": ""
+    })
+    return render_template("index.html", data=data)
 
-@app.route("/search", methods = ['POST'])
+
+@app.route("/search", methods=['POST'])
 def search():
     if request.method == 'POST':
-      query = request.form['search']
-    data = client.search(engine_name, query, {})
-    return render_template("results.html" , data=data)
-     
+        query = request.form['search']
+    data = client.search(engine_name, body={
+        "query": query
+    })
+    return render_template("index.html", data=data)
+
+
 @app.route("/index")
 def index():
     # Opening JSON file 
-    f = open('data.json',) 
+    f = open('data.json', )
 
     # returns JSON object as  
     # a dictionary 
     documents = json.load(f)
     data = client.index_documents(engine_name, documents)
-    return render_template("about.html" , data=data)
-    
+    return render_template("about.html", data=data)
+
+
 if __name__ == "__main__":
     app.run(debug=False)
