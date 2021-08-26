a147aeee80b4ce2a9f53d068b4276c763fe86936
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-08-26 20:42:10 +0530

    APM + ECS Logging to Elasticsearch

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # app-search-flask-app
  This is a example of Elastic App Search with App Search Python Client
+
+# Running App
+
+> gunicorn --workers 2 --bind 0.0.0.0:5000 main:app --log-level info --error-logfile app.log --capture-output
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 
 from elastic_enterprise_search import AppSearch
 from flask import Flask, render_template, request
+from elasticapm.contrib.flask import ElasticAPM
+import logging
+import ecs_logging
 
 app = Flask(__name__)
 
@@ -12,11 +15,17 @@ client = AppSearch(
     config['appsearch']['base_endpoint'],
     http_auth=config['appsearch']['api_key'])
 
+apm = ElasticAPM(app, logging=logging.INFO,
+                 server_url=config['apm']['server_url'],
+                 service_name=config['apm']['service_name'],
+                 secret_token=config['apm']['secret_token'])
+
 engine_name = config['appsearch']['engine_name']
 
 
 @app.route("/")
 def home():
+    app.logger.info('Welcome to the Movies App!')
     data = client.search(engine_name, body={
         "query": ""
     })
@@ -27,6 +36,7 @@ def home():
 def search():
     if request.method == 'POST':
         query = request.form['search']
+        app.logger.info('Calling Search with query: %s', query)
     data = client.search(engine_name, body={
         "query": query
     })
@@ -46,4 +56,15 @@ def index():
 
 
 if __name__ == "__main__":
+    handler = logging.StreamHandler()
+    handler.setFormatter(ecs_logging.StdlibFormatter())
+    app.logger.addHandler(handler)
     app.run(debug=False)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    handler = logging.StreamHandler()
+    handler.setFormatter(ecs_logging.StdlibFormatter())
+    app.logger.addHandler(handler)
+    app.logger.setLevel(gunicorn_logger.level)
